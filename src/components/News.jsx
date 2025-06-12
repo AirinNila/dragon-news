d91dd6ff966c3a9f5a61431190956a0e79e4d022
AirinNila <airinakhternila>
2025-06-12 21:15:22 +0600

    added news card

---
+++ src/components/News.jsx
@@ -0,0 +1,47 @@
+import { CiShare2, CiBookmark } from "react-icons/ci";
+
+const News = ({ data }) => {
+    const { title, author, image_url, details } = data;
+    return (
+        <div className="flex flex-col">
+            <div className="bg-gray-100 p-4 rounded-tl-selector rounded-tr-lg flex justify-between items-center">
+                <div className="flex items-center gap-2">
+                    <img src={author.img} alt="" className="rounded-full w-10 md:w-16" />
+                    <div className="flex flex-col items-start justify-center gap-1 md:gap-2">
+                        <h2 className="font-bold">{author.name}</h2>
+                        <p className="des text-sm md:text-md">{author.published_date}</p>
+                    </div>
+                </div>
+
+                <div className="flex gap-2 items-center text-md md:text-3xl">
+                    <CiShare2 />
+                    <CiBookmark />
+                </div>
+            </div>
+            <div className="card rounded-0 border-2 border-gray-100 p-2 md:p-4 w-full">
+                <div className="card-body">
+
+                    <p className="card-title">{title}</p>
+                </div>
+                <figure>
+                    <img
+                        src={image_url}
+                        alt="Shoes" className="rounded-md" />
+                </figure>
+                <p className="text-base des mt-6">{details}</p>
+                <div className="border-t-2 p-3 border-t-gray-100 mt-3">
+                    <div className="rating">
+                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="1 star" />
+                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="2 star"  />
+                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="3 star" defaultChecked />
+                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="4 star" />
+                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="5 star" />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default News;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import LeftSideNav from "../../components/LeftSideNav";
 import Navbar from "../../components/Navbar";
 import RightSideNav from "../../components/RightSideNav";
 import Title from "../../components/Title";
+import News from "../../components/news";
 
 const Home = () => {
+    const [news, setNews] = useState([])
+
+    useEffect( () => {
+        fetch('news.json')
+        .then(res => res.json())
+        .then(data => setNews(data))
+    },[])
     return (
         <div className="flex flex-col gap-4 mt-4">
             <Header></Header>
@@ -14,7 +23,12 @@ const Home = () => {
                 <div>
                     <LeftSideNav></LeftSideNav>
                 </div>
-                <div className="col-span-2"></div>
+                <div className="col-span-2 flex flex-col gap-4">
+                    <p className="font-extrabold">Dragon News Home</p>
+                    {
+                        news.map(data => <News key={data.id} data={data}></News>)
+                    }
+                </div>
                 <div>
                     <RightSideNav></RightSideNav>
                 </div>
